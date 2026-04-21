62d861354738378b5cad4d6e73cbec4674d305da
Matthew Leong <Matthew.Leong@bankofengland.co.uk>
2026-04-21 17:40:26 +0100

    fixing main scripts

--- scripts/iterate_folder.py
+++ scripts/iterate_folder.py
@@ -6,7 +6,8 @@ import json
 import logging.handlers
 import os
 import sys
-from datetime import datetime
+from datetime import datetime, timezone
+from pathlib import Path
 
 import zstandard
 
@@ -16,22 +17,25 @@ log.addHandler(logging.StreamHandler())
 
 
 def read_and_decode(reader, chunk_size, max_window_size, previous_chunk=None, bytes_read=0):
+    """Reads a chunk of data from the reader and attempts to decode it. If a UnicodeDecodeError occurs, it will read another chunk and try again until it can decode the data or it has read more than the max_window_size."""
     chunk = reader.read(chunk_size)
     bytes_read += chunk_size
     if previous_chunk is not None:
         chunk = previous_chunk + chunk
     try:
         return chunk.decode()
-    except UnicodeDecodeError:
+    except UnicodeDecodeError as e:
         if bytes_read > max_window_size:
             msg = f"Unable to decode frame after reading {bytes_read:,} bytes"
-            raise UnicodeError(msg)
-        log.info(f"Decoding error with {bytes_read:,} bytes, reading another chunk")
+            raise UnicodeError(msg) from e
+        msg = f"Decoding error with {bytes_read:,} bytes, reading another chunk"
+        log.info(msg)
         return read_and_decode(reader, chunk_size, max_window_size, chunk, bytes_read)
 
 
 def read_lines_zst(file_name):
-    with open(file_name, "rb") as file_handle:
+    """Reads lines from a zst compressed ndjson file."""
+    with Path(file_name).open("rb") as file_handle:
         buffer = ""
         reader = zstandard.ZstdDecompressor(max_window_size=2**31).stream_reader(file_handle)
         while True:
@@ -42,7 +46,7 @@ def read_lines_zst(file_name):
             lines = (buffer + chunk).split("\n")
 
             for line in lines[:-1]:
-                yield line.strip(), file_handle.tell()
+                yield line, file_handle.tell()
 
             buffer = lines[-1]
 
@@ -54,13 +58,13 @@ input_files = []
 total_size = 0
 for subdir, _dirs, files in os.walk(input_folder):
     for filename in files:
-        input_path = os.path.join(subdir, filename)
-        if input_path.endswith(".zst"):
-            file_size = os.stat(input_path).st_size
+        input_path = Path(subdir) / filename
+        if input_path.suffix == ".zst":
+            file_size = input_path.stat().st_size
             total_size += file_size
             input_files.append([input_path, file_size])
-
-log.info(f"Processing {len(input_files)} files of {(total_size / (2**30)):.2f} gigabytes")
+msg = f"Processing {len(input_files)} files of {(total_size / (2**30)):.2f} gigabytes"
+log.info(msg)
 
 total_lines = 0
 total_bytes_processed = 0
@@ -70,14 +74,17 @@ for input_file in input_files:
     created = None
     for line, file_bytes_processed in read_lines_zst(input_file[0]):
         obj = json.loads(line)
-        created = datetime.utcfromtimestamp(int(obj["created_utc"]))
+        created = datetime.fromtimestamp(int(obj["created_utc"]), tz=timezone.utc)
         file_lines += 1
         if file_lines == 1:
-            log.info(f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines + total_lines:,} : 0% : {(total_bytes_processed / total_size) * 100:.0f}%")
+            msg = f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines + total_lines:,} : 0% : {(total_bytes_processed / total_size) * 100:.0f}%"
+            log.info(msg)
         if file_lines % 100000 == 0:
-            log.info(f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines + total_lines:,} : {(file_bytes_processed / input_file[1]) * 100:.0f}% : {(total_bytes_processed / total_size) * 100:.0f}%")
+            msg = f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines + total_lines:,} : {(file_bytes_processed / input_file[1]) * 100:.0f}% : {(total_bytes_processed / total_size) * 100:.0f}%"
+            log.info(msg)
     total_lines += file_lines
     total_bytes_processed += input_file[1]
-    log.info(f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {total_lines:,} : 100% : {(total_bytes_processed / total_size) * 100:.0f}%")
-
-log.info(f"Total: {total_lines}")
+    msg = f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {total_lines:,} : 100% : {(total_bytes_processed / total_size) * 100:.0f}%"
+    log.info(msg)
+msg = f"Total: {total_lines}"
+log.info(msg)
--- scripts/single_file.py
+++ scripts/single_file.py
@@ -2,9 +2,9 @@
 
 import json
 import logging.handlers
-import os
 import sys
-from datetime import datetime
+from datetime import datetime, timezone
+from pathlib import Path
 
 import zstandard
 
@@ -14,22 +14,25 @@ log.addHandler(logging.StreamHandler())
 
 
 def read_and_decode(reader, chunk_size, max_window_size, previous_chunk=None, bytes_read=0):
+    """Reads a chunk of data from the reader and attempts to decode it. If a UnicodeDecodeError occurs, it will read another chunk and try again until it can decode the data or it has read more than the max_window_size."""
     chunk = reader.read(chunk_size)
     bytes_read += chunk_size
     if previous_chunk is not None:
         chunk = previous_chunk + chunk
     try:
         return chunk.decode()
-    except UnicodeDecodeError:
+    except UnicodeDecodeError as e:
         if bytes_read > max_window_size:
             msg = f"Unable to decode frame after reading {bytes_read:,} bytes"
-            raise UnicodeError(msg)
-        log.info(f"Decoding error with {bytes_read:,} bytes, reading another chunk")
+            raise UnicodeError(msg) from e
+        msg = f"Decoding error with {bytes_read:,} bytes, reading another chunk"
+        log.info(msg)
         return read_and_decode(reader, chunk_size, max_window_size, chunk, bytes_read)
 
 
 def read_lines_zst(file_name):
-    with open(file_name, "rb") as file_handle:
+    """Reads lines from a zst compressed ndjson file."""
+    with Path(file_name).open("rb") as file_handle:
         buffer = ""
         reader = zstandard.ZstdDecompressor(max_window_size=2**31).stream_reader(file_handle)
         while True:
@@ -49,27 +52,25 @@ def read_lines_zst(file_name):
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
-    file_size = os.stat(file_path).st_size
+    file_size = Path(file_path).stat().st_size
     file_lines = 0
     file_bytes_processed = 0
     created = None
     field = "subreddit"
     value = "wallstreetbets"
     bad_lines = 0
-    # try:
     for line, file_bytes_processed in read_lines_zst(file_path):
         try:
             obj = json.loads(line)
-            created = datetime.utcfromtimestamp(int(obj["created_utc"]))
+            created = datetime.fromtimestamp(int(obj["created_utc"]), tz=timezone.utc)
             temp = obj[field] == value
         except (KeyError, json.JSONDecodeError):
             bad_lines += 1
         file_lines += 1
         if file_lines % 100000 == 0:
-            log.info(f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines:,} : {bad_lines:,} : {file_bytes_processed:,}:{(file_bytes_processed / file_size) * 100:.0f}%")
+            msg = f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines:,} : {bad_lines:,} : {file_bytes_processed:,}:{(file_bytes_processed / file_size) * 100:.0f}%"
+            log.info(msg)
 
-    # except Exception as err:
-    #     log.info(err)
-
-    log.info(f"Complete : {file_lines:,} : {bad_lines:,}")
+    msg = f"Complete : {file_lines:,} : {bad_lines:,}"
+    log.info(msg)
 
